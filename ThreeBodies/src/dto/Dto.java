6b70c2c474bf710cc2b206ccfe44f6e8cae841fb
Sissel-Wu <chengbutang@163.com>
2015-04-17 00:38:25 +0800

    初步架构
    争取做好分层，部署到服务器的代码，PC端的代码，安卓端的代码（假如有时间）之间做到最大程度共用
    
    dto包中的Dto类是用来存放软件运行时的数据的
    model包放模型
    ui包做界面，假如我们分层做得好，开发安卓端只要重写这个包就可以了
    data包负责持久化用户的数据，与服务器端交互
    config包负责 用户的个性化设置，游戏的参数。打算用xml实现。
    control包就是负责整个软件的流程，包括各个界面的转换，游戏的进行等等
    ai包是写ai的地方，因为这个技术含量高所以拿出来 = =

---
+++ ThreeBodies/src/dto/Dto.java
@@ -0,0 +1,15 @@
+package dto;
+
+import java.util.List;
+
+import ui.BGM;
+
+public class Dto {
+    
+    private List<Character> characters;
+    private int whoseTurn;
+    private int volume;
+    private BGM bgm;
+    
+
+}
---
+++ ThreeBodies/src/model/User.java
@@ -0,0 +1,11 @@
+package model;
+
+public class User {
+
+    private String name;
+    private String id;
+    private String password;
+    private String country;
+    private String email;
+    
+}
