baa06c4d3c27f68e7d3614520c26946fa2319ea3
ShinAnn <wzian@126.com>
2015-05-11 19:27:37 +0800

    。。

--- ThreeBody/src/control/MainControl.java
+++ ThreeBody/src/control/MainControl.java
@@ -1,11 +1,8 @@
 package control;
 
-import io.NetClient;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import dto.AccountDTO;
 import ui.AboutUsPanel;
 import ui.LobbyPanel;
 import ui.MainFrame;
@@ -15,6 +12,7 @@ import ui.game.GamePanel;
 import ui.sound.Media;
 import ui.sound.Sound;
 import util.R;
+import dto.AccountDTO;
 
 public class MainControl {
 
