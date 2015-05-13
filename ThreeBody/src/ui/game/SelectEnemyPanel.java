0f798d5f06aa6ba143124d3876edb8de85914798
ShinAnn <wzian@126.com>
2015-05-13 23:25:42 +0800

    电波干扰和特权的弹窗

--- ThreeBody/src/ui/game/SelectEnemyPanel.java
+++ ThreeBody/src/ui/game/SelectEnemyPanel.java
@@ -1,4 +1,4 @@
-package ui;
+package ui.game;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -6,28 +6,35 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import ui.InformFrame;
+import model.Player;
+import dto.GameDTO;
 
-public class MessageByFramePanel extends JPanel{
+public class SelectEnemyPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private JButton btnOK;
+	private JComboBox<String> enemys;
 	private JLabel msgLabel;
+	List<Player> players=null;
+	Player user;
 	/**
 	 * 
-	 * @param i 1代表login，2代表logup
-	 * @param successInformFrame O
+	 * @param successInformFrame 
 	 */
-	public MessageByFramePanel(InformFrame successInformFrame,String message) {
+	public SelectEnemyPanel(JFrame frame,String message) {
 		this.setLayout(null);
-		this.frame=successInformFrame;
+		players= GameDTO.getInstance().getPlayers();
+		user=GameDTO.getInstance().getUser();
+		this.frame=frame;
 		this.initComonent(message);
 	}
 	private void initComonent(String message) {
@@ -41,9 +48,26 @@ public class MessageByFramePanel extends JPanel{
 		msgLabel.setForeground(Color.YELLOW);
 		msgLabel.setText(message);
 		msgLabel.setFont(new Font("宋体", Font.BOLD, 20));
-		msgLabel.setBounds(60,10,180,80);
+		msgLabel.setBounds(60,0,180,80);
 		this.add(msgLabel);
-
+		
+		
+		enemys = new JComboBox<String>();
+		enemys.setFont(new Font("宋体", Font.PLAIN, 30));
+		enemys.setBounds(120,60,60,30);
+		//
+		if (players != null) {
+			for (int i = 0; i < players.size(); i++) {
+				if (players.get(i).equals(user)) {
+					continue;
+				} else {
+					enemys.addItem(players.get(i).getAccount().getId());
+				}
+			}
+		}
+		enemys.addItem("aa");
+		enemys.addItem("bb");
+		this.add(enemys);
 		
 	}
 	
@@ -71,6 +95,6 @@ public class MessageByFramePanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		Image IMG_MAIN = new ImageIcon("images/img1.jpg").getImage();
 		// 绘制游戏界面
-		g.drawImage(IMG_MAIN, 0, 0,695,215, null);
+		g.drawImage(IMG_MAIN, 0, 0, null);
 	}
 }
